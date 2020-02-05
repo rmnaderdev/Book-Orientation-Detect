f7079110b9db66a0e9b1301e2970051498fa5a68
RJ <rmnader@svsu.edu>
2020-02-05 15:25:34 -0500

    Cleaned things up.

---
+++ ImageClassificationTrain/Common/FileUtils.cs
@@ -0,0 +1,54 @@
+﻿using Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Common
+{
+    public class FileUtils
+    {
+        public static IEnumerable<(string imagePath, string label)> LoadImagesFromDirectory(string folder, bool useFolderNameasLabel)
+        {
+            var imagesPath = Directory
+                .GetFiles(folder, "*", searchOption: SearchOption.AllDirectories)
+                .Where(x => Path.GetExtension(x) == ".jpg" || Path.GetExtension(x) == ".png");
+
+
+            if(useFolderNameasLabel)
+            {
+                return imagesPath.Select(imagePath => (imagePath, Directory.GetParent(imagePath).Name));
+            }
+            else
+            {
+                return imagesPath.Select(imagePath =>
+                {
+                    var label = Path.GetFileName(imagePath);
+                    for (var index = 0; index < label.Length; index++)
+                    {
+                        if (!char.IsLetter(label[index]))
+                        {
+                            label = label.Substring(0, index);
+                            break;
+                        }
+                    }
+                    return (imagePath, label);
+                });
+            }
+        }
+
+        public static IEnumerable<InMemoryImageData> LoadInMemoryImagesFromDirectory(string folder, bool useFolderNameAsLabel = true)
+            => LoadImagesFromDirectory(folder, useFolderNameAsLabel)
+                .Select(x => new InMemoryImageData(
+                    image: File.ReadAllBytes(x.imagePath),
+                    label: x.label,
+                    imageFileName: Path.GetFileName(x.imagePath)));
+
+        public static string GetAbsolutePath(Assembly assembly, string relativePath)
+        {
+            var assemblyFolderPath = new FileInfo(assembly.Location).Directory.FullName;
+
+            return Path.Combine(assemblyFolderPath, relativePath);
+        }
+    }
+}
---
+++ ImageClassificationTrain/Models/ImagePrediction.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class ImagePrediction
+    {
+        [ColumnName("Score")]
+        public float[] Score;
+
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabel;
+    }
+}

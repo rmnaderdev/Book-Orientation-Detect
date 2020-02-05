5425555d1c36aa5671b7a67c7e1524aa160db234
RJ <rmnader@svsu.edu>
2020-02-05 14:33:53 -0500

    Added test project.

---
+++ ImageClassificationTest/ImageData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageClassificationTest
+{
+    public class ImageData
+    {
+
+        public readonly string ImagePath;
+        public readonly string Label;
+
+        public ImageData(string imagePath, string label)
+        {
+            ImagePath = imagePath;
+            Label = label;
+        }
+    }
+}
---
+++ ImageClassificationTest/InMemoryImageData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageClassificationTest
+{
+    public class InMemoryImageData
+    {
+        public readonly byte[] Image;
+        public readonly string Label;
+        public readonly string ImageFileName;
+
+        public InMemoryImageData(byte[] image, string label, string imageFileName)
+        {
+            Image = image;
+            Label = label;
+            ImageFileName = imageFileName;
+        }
+    }
+}
